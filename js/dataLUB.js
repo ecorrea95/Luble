b21b8e54f166630f5930e76bde5416d8a8714977
ecorrea95 <e.correa95@hotmail.com>
2022-04-04 20:26:32 -0300

    Iniciado relevo de datos de jugadores

---
+++ js/dataLUB.js
@@ -0,0 +1,88 @@
+// Data jugadores LUB
+const jugadores = [
+  // Aguada
+  {
+    nombre: "Juan Ignacio Stoll",
+    imagen: "images/juanignaciostoll.jpg",
+    equipoActual: "Aguada",
+    logoEquipo: "images/aguada.png",
+    equiposPasado: ["Aguada"],
+    posicion: "B",
+    altura: "187cm",
+    numero: "3",
+    edad: "18",
+    extranjero: "No"
+  },
+  {
+    nombre: "Marcos Mata",
+    imagen: "images/marcosmata.jpg",
+    equipoActual: "Aguada",
+    logoEquipo: "images/aguada.png",
+    equiposPasado: ["Aguada"],
+    posicion: "AL",
+    altura: "202cm",
+    numero: "6",
+    edad: "35",
+    extranjero: "Si"
+  },
+  {
+    nombre: "Sebastian Izaguirre",
+    imagen: "images/sebastianizaguirre.jpg",
+    equipoActual: "Aguada",
+    logoEquipo: "images/aguada.png",
+    equiposPasado: ["Paysandú", "Trouville", "Hebraica y Macabi", "Bohemios", "Capitol", "Defensor Sporting", "Unión Atlética", "Aguada"],
+    posicion: "AP",
+    altura: "207cm",
+    numero: "10",
+    edad: "36",
+    extranjero: "No"
+  },
+  {
+    nombre: "Leandro Garcia Morales",
+    imagen: "images/leandrogarciamorales.jpg",
+    equipoActual: "Aguada",
+    logoEquipo: "images/aguada.png",
+    equiposPasado: ["Biguá", "Aguada", "Hebraica y Macabi"],
+    posicion: "ES",
+    altura: "188cm",
+    numero: "11",
+    edad: "41",
+    extranjero: "No"
+  },
+  {
+    nombre: "",
+    imagen: "images/",
+    equipoActual: "",
+    logoEquipo: "images/",
+    equiposPasado: [""],
+    posicion: "",
+    altura: "",
+    numero: "",
+    edad: "",
+    extranjero: ""
+  },
+  {
+    nombre: "",
+    imagen: "images/",
+    equipoActual: "",
+    logoEquipo: "images/",
+    equiposPasado: [""],
+    posicion: "",
+    altura: "",
+    numero: "",
+    edad: "",
+    extranjero: ""
+  },
+  {
+    nombre: "",
+    imagen: "images/",
+    equipoActual: "",
+    logoEquipo: "images/",
+    equiposPasado: [""],
+    posicion: "",
+    altura: "",
+    numero: "",
+    edad: "",
+    extranjero: ""
+  },
+]
