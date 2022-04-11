ea6ae72c9a118db0c7c8d84b21323346cfdce84a
ecorrea95 <e.correa95@hotmail.com>
2022-04-11 15:06:38 -0300

    Agregado style modal stats

--- js/localstorage.js
+++ js/localstorage.js
@@ -57,23 +57,48 @@ if (diaActual != diaGuardado || mesActual != mesGuardado) {
 
 }
 
-// Traigo todos los stats
-var statCantPart = localStorage.getItem('cantidadPartidas');
-var statCantPartG = localStorage.getItem('cantPartGanadas');
-var statCantPartP = localStorage.getItem('cantPartPerdidas');
-var gan1 = localStorage.getItem('ganada1');
-var gan2 = localStorage.getItem('ganada2');
-var gan3 = localStorage.getItem('ganada3');
-var gan4 = localStorage.getItem('ganada4');
-var gan5 = localStorage.getItem('ganada5');
-var gan6 = localStorage.getItem('ganada6');
+$(document).ready(function() {
+  // Traigo todos los stats
+  var statCantPart = localStorage.getItem('cantidadPartidas');
+  $("#valCantJug").text(statCantPart);
 
-console.log("Cantidad partidas: ", statCantPart);
-console.log("Cantidad partidas ganadas: ", statCantPartG);
-console.log("Cantidad partidas perdidas: ", statCantPartP);
-console.log("Ganadas en 1: ", gan1);
-console.log("Ganadas en 2: ", gan2);
-console.log("Ganadas en 3: ", gan3);
-console.log("Ganadas en 4: ", gan4);
-console.log("Ganadas en 5: ", gan5);
-console.log("Ganadas en 6: ", gan6);
+  var statCantPartG = localStorage.getItem('cantPartGanadas');
+  $("#valCantGan").text(statCantPartG/statCantPart*100+"%");
+
+  var statCantPartP = localStorage.getItem('cantPartPerdidas');
+  $("#valCantPerd").text(statCantPartP/statCantPart*100+"%");
+
+  var gan1 = localStorage.getItem('ganada1');
+  $("#prog1").val(gan1/statCantPartG*100);
+  $("#prog1Val").text(gan1/statCantPartG*100+"%");
+
+  var gan2 = localStorage.getItem('ganada2');
+  $("#prog2").val(gan2/statCantPartG*100);
+  $("#prog2Val").text(gan2/statCantPartG*100+"%");
+
+  var gan3 = localStorage.getItem('ganada3');
+  $("#prog3").val(gan3/statCantPartG*100);
+  $("#prog3Val").text(gan3/statCantPartG*100+"%");
+
+  var gan4 = localStorage.getItem('ganada4');
+  $("#prog4").val(gan4/statCantPartG*100);
+  $("#prog4Val").text(gan4/statCantPartG*100+"%");
+
+  var gan5 = localStorage.getItem('ganada5');
+  $("#prog5").val(gan5/statCantPartG*100);
+  $("#prog5Val").text(gan5/statCantPartG*100+"%");
+
+  var gan6 = localStorage.getItem('ganada6');
+  $("#prog6").val(gan6/statCantPartG*100);
+  $("#prog6Val").text(gan6/statCantPartG*100+"%");
+
+  console.log("Cantidad partidas: ", statCantPart);
+  console.log("Cantidad partidas ganadas: ", statCantPartG);
+  console.log("Cantidad partidas perdidas: ", statCantPartP);
+  console.log("Ganadas en 1: ", gan1);
+  console.log("Ganadas en 2: ", gan2);
+  console.log("Ganadas en 3: ", gan3);
+  console.log("Ganadas en 4: ", gan4);
+  console.log("Ganadas en 5: ", gan5);
+  console.log("Ganadas en 6: ", gan6);
+});
